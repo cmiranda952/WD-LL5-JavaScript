a99a0d8841c9be5ebab17fdc34a10a23542d2998
Carlos Miranda Jr <carlos.miranda2@my.utsa.edu>
2026-02-20 01:06:30 +0000

    Playing arround with JavaScript

---
+++ script.js
@@ -0,0 +1,23 @@
+console.time('data processing');
+console.log("Hello, World!");
+console.error("This is an error.");
+alert("Welcome to my site!");
+
+console.log(console);
+console.log(document);
+
+console.log("Hello, World!");
+
+const users = [
+  {name: "John Doe", age: 30 },
+  {name: "Jane Smith", age: 28 },
+  {name: "Bob Johnson", age: 35 }
+];
+
+const fruits = ["Apples", "Oranges", "Bananas"];
+
+console.table(users);
+console.table(fruits);
+console.count(console.log("Hello, World!"));
+
+console.timeEnd('data processing'); 
